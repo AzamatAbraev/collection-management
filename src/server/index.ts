bc85e38b11c3b0ead59c39bdc210ad20d5c2267c
Your Name <azamatabraev07@gmail.com>
2024-03-23 03:53:48 +0500

    base url changed

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,8 +4,8 @@ import { TOKEN } from "../constants";
 import { message } from "antd";
 
 const request = axios.create({
-  // baseURL: "https://collection-management-server.up.railway.app/api/v1/",
-  baseURL: "http://localhost:3000/api/v1/",
+  baseURL: "https://collection-management-server.up.railway.app/api/v1/",
+  // baseURL: "http://localhost:3000/api/v1/",
   timeout: 15000,
   headers: { Authorization: `Bearer ${Cookies.get(TOKEN)}` },
 });
